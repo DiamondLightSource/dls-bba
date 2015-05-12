51600d1376c74f3620e27d4d83b83dd20783468e
Matthew Furseman <matthew.furseman@diamond.ac.uk>
2015-05-12 12:57:25 +0100

    Analysis: Remove benchmarking

--- analysis.py
+++ analysis.py
@@ -102,8 +102,7 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    tic = time.time()  # BENCHMARKING
-    print analyse(
-            io.loadmat(args.filename, squeeze_me=True), plot_output=args.plot)
-    print 'Took', time.time() - tic, 'seconds'  # BENCHMARKING
+    analyse(
+            io.loadmat(args.filename, squeeze_me=True),
+            plot_output=args.plot, use_fft=False)
 
