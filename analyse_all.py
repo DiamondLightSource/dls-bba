0bf522021715d99a3e5b4e9b35a6c2bf6ad13637
Will Rogers <wahrogers@gmail.com>
2015-10-12 11:09:47 +0100

    Use simple_analysis.py in analyse_all.
    * Pass directory as argument to analyse_all.py
    * Tidy imports

--- analyse_all.py
+++ analyse_all.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env dls-python
 
-import analysis
+import pkg_resources
+pkg_resources.require('scipy')
+pkg_resources.require('numpy')
+import simple_analysis
 import scipy.io
 import numpy
 import os
+import sys
 
-DATADIR = 'data'
 
-files = os.listdir(DATADIR)
+try:
+    datadir = sys.argv[1]
+except IndexError:
+    print('Usage: {} <datadir>'.format(sys.argv[0]))
+    sys.exit()
 
 results = []
 for f in files:
     if f.endswith('mat'):
-        f = os.path.join(DATADIR, f)
+        f = os.path.join(datadir, f)
         data = scipy.io.loadmat(f, squeeze_me=True)
-        results.append(analysis.analyse(data, False))
+        results.append(simple_analysis.analyse(data))
 
 a = numpy.array(results)
 
