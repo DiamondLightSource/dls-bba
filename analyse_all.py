f08e94b25bfd64fb8b74fb7699b9be9b99f02b17
Will Rogers <wahrogers@gmail.com>
2015-10-20 15:39:56 +0100

    analyse_all.py: move to using functions.

--- analyse_all.py
+++ analyse_all.py
@@ -12,27 +12,31 @@ import sys
 import glob
 
 
-try:
-    datadir = sys.argv[1]
-except IndexError:
-    print('Usage: {} <datadir>'.format(sys.argv[0]))
-    sys.exit()
+def analyse_all(datadir):
+    files = glob.glob(os.path.join(datadir, '*.mat'))
+
+    print('Analysing {}: {} files.'.format(datadir, len(files)))
+
+    for module in (simple_analysis, analysis):
+        print('Analysing with {}'.format(module.__name__))
+        results = []
+        for f in files:
+            data = scipy.io.loadmat(f, squeeze_me=True)
+            results.append(module.analyse(data))
+
+        a = numpy.array(results)
+
+        print a.mean()
+        print a.std()
+        print('')
 
 
-files = glob.glob(os.path.join(datadir, '*.mat'))
+if __name__ == '__main__':
+    try:
+        datadir = sys.argv[1]
+    except IndexError:
+        print('Usage: {} <datadir>'.format(sys.argv[0]))
+        sys.exit()
 
-print('Analysing {}: {} files.'.format(datadir, len(files)))
-
-for module in (simple_analysis, analysis):
-    print('Analysing with {}'.format(module.__name__))
-    results = []
-    for f in files:
-        data = scipy.io.loadmat(f, squeeze_me=True)
-        results.append(module.analyse(data))
-
-    a = numpy.array(results)
-
-    print a.mean()
-    print a.std()
-    print('')
+    analyse_all(datadir)
 
