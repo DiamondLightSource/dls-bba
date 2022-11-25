67d94f1d05ed914b4bf4b48997019944c6eacb14
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2022-11-25 11:34:43 +0000

    Add offset suffixes, RawData and Results.

--- bba/common.py
+++ bba/common.py
@@ -15,6 +15,49 @@ PLANE_VALUES = {
     "HORIZONTAL": PlaneValues(0, "X", "HSTR", "x_kick"),
     "VERTICAL": PlaneValues(1, "Y", "VSTR", "y_kick")}
 
+ORIGIN_SUFFIXES = {
+    "BBA": ":CF:BBA_{axis}_S",
+    "BCD": ":CF:BCD_{axis}_S",
+    "GOLDEN": ":CF:GOLDEN_{axis}_S"}
+
+
+@dataclass
+class RawData:
+    raw_data: Dict[str, Any]
+    algorithm: str
+    metadata: Dict[str, Any]
+
+    # TODO: asdict, make all shared attributes not in metadata.
+
+    def save(self, time_prefix, filepath = "data"):
+        dct = {'raw_data': self.raw_data, 'algorithm': self.algorithm, 'metadata': self.metadata}
+        filename = "{}/{}-{}-{}-rawdata".format(filepath, time_prefix, self.metadata["bpm"][0], self.metadata["plane"].axis)
+        io.savemat(filename, dct, oned_as="row")
+        print(f"Saved data as {filename}")
+
+    @classmethod
+    def from_file(cls, filename):
+        dct = io.loadmat(filename, squeeze_me=True)
+        return cls(dct['raw_data'], dct['algorithm'], dct['metadata'])
+
+
+@dataclass
+class Results:
+    results: Dict[str, Any]
+    bpm_pv_prefix: str
+    metadata: Dict[str, Any]
+
+    def save(self, time_prefix, filepath = "data"):
+        dct = {'results': self.results, 'bpm_pv_prefix': self.bpm_pv_prefix, 'metadata': self.metadata}
+        filename = "{}/{}-{}-{}-results".format(filepath, time_prefix, self.metadata["bpm"][0], self.metadata["plane"].axis)
+        io.savemat(filename, dct, oned_as="row")
+        print(f"Saved data as {filename}")
+    
+    @classmethod
+    def from_file(cls, filename):
+        dct = io.loadmat(filename, squeeze_me=True)
+        return cls(dct['results'], dct['bpm_pv_prefix'], dct['metadata'])
+
 
 class Algorithm(ABC):
     def __init__(self, accelerator):
