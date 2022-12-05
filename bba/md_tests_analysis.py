8b937e29c5eaca3e3f46e203eefeb1ba11215616
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2022-12-05 10:55:48 +0000

    Updated testing

--- bba/md_tests_analysis.py
+++ bba/md_tests_analysis.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 import argparse
 import logging as log
-from statistics import mean
+from statistics import mean, stdev
 
 from bba.common import Algorithm, PLANE_VALUES
 from bba.fbba import FBBA
@@ -16,205 +16,176 @@ import numpy as np
 def parse_args():
     parser = argparse.ArgumentParser(description="testing")
     parser.add_argument(
-        "-c",
-        "-cycle",
-        dest = "cycle",
+        "-c", "--cycle",
+        dest="cycle",
         action="store_true",
         default=False,
-        help=""
+        help="Cycle test"
     )
     parser.add_argument(
-        "-t",
-        "-correctors",
-        dest = "corr_cycles_test",
+        "-f", "--freq",
+        dest="freq",
         action="store_true",
         default=False,
-        help=""
+        help="Freq test"
     )
     parser.add_argument(
-        "-j",
-        "-honing",
-        dest = "honing_test",
+        "-q", "--quads",
+        dest="quads_amp_test",
         action="store_true",
         default=False,
-        help=""
+        help="Quad test"
     )
     parser.add_argument(
-        "-s",
-        "-simple",
-        dest = "simple",
+        "-k", "--corrector",
+        dest="corr_amp_test",
         action="store_true",
         default=False,
-        help=""
+        help="Corrector test"
+    )
+    parser.add_argument(
+        "-j", "--honing",
+        dest="honing_test",
+        action="store_true",
+        default=False,
+        help="honing complex test"
+    )
+    parser.add_argument(
+        "-s", "--special",
+        dest="combi",
+        action="store_true",
+        default=False,
+        help="combination test"
     )
     return parser.parse_args()
     
 
 def main():
     args = parse_args()
-    cycle = args.cycle
-    corr_cycles_test = args.corr_cycles_test
+    cycle_test = args.cycle
+    freq_test = args.freq
+    corr_test = args.corr_amp_test
+    quad_test = args.quad_amp_test
     honing_test = args.honing_test
-    simple = args.simple
+    combination_test = args.combi
 
-
-    if cycle:
-        matrix = np.genfromtxt("md_data/cycle_test.csv", delimiter=",")
-        #print(matrix)
-        cycle_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    if cycle_test:
+        repeats = 20
+        cycle_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+        data = np.genfromtxt(f"cycle_scan_repeats_{repeats}_len_{len(cycle_list)}.csv", delimiter=",")
         y = []
-        yerr = []
-
-        for index, cycle in enumerate(cycle_numbers):
-            value_list = matrix[(index*2), :]
-            error_list = matrix[(index*2) + 1, :]
-            value_list = [i for i in value_list if i != 0]
-            error_list = [i for i in error_list if i != 0]
-            y_value = mean(value_list)
-            y.append(y_value)
-            sum_error = 0
-            for index, error in enumerate(error_list):
-                sum_error += (error/value_list[index])**2
-            sum_error = np.sqrt(sum_error)
-            yerr.append(sum_error * y_value)
-
-        #print(y, yerr)
-
-        plt.errorbar(cycle_numbers, y, yerr, marker = ".", capsize=5)
-        plt.title("Number of Cycles test - 20 Repeats")
-        plt.xlim(0,51)
-        plt.xlabel("Cycle number")
+        y_err = []
+        for index, cycle in enumerate(cycle_list):
+            value_list = data[(index*2), :]
+            y.append(mean(value_list))
+            y_err.append(stdev(value_list))
+        plt.errorbar(cycle_list, y, y_err, marker = ".", capsize=5)
+        plt.title(f"Cycles Test - {repeats} Repeats")
+        plt.xlim(0, max(cycle_list) + 1)
+        plt.xlabel("Cycles")
         plt.ylabel("Offset Value")
         plt.grid(which = "both", axis = "both")
-        plt.savefig("cyclestest.png", bbox_inches="tight", dpi=1200)
+        plt.savefig(f"cycle_scan_repeats_{repeats}_len_{len(cycle_list)}_plot.png", bbox_inches="tight", dpi=1200)
         plt.show()
 
+    if freq_test:
+        repeats = 10
+        frequency_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+        data = np.genfromtxt(f"frequency_scan_repeats_{repeats}_len_{len(frequency_list)}.csv", delimiter=",")
+        y = []
+        y_err = []
+        for index, cycle in enumerate(frequency_list):
+            value_list = data[(index*2), :]
+            y.append(mean(value_list))
+            y_err.append(stdev(value_list))
+        plt.errorbar(frequency_list, y, y_err, marker = ".", capsize=5)
+        plt.title(f"Frequency Test - {repeats} Repeats")
+        plt.xlim(0, max(frequency_list) + 1)
+        plt.xlabel("Frequency")
+        plt.ylabel("Offset Value")
+        plt.grid(which = "both", axis = "both")
+        plt.savefig(f"frequency_scan_repeats_{repeats}_len_{len(frequency_list)}_plot.csv.png", bbox_inches="tight", dpi=1200)
+        plt.show()
 
-    if corr_cycles_test:
-        matrix1 = np.genfromtxt("corr1_cycle_test.csv", delimiter=",")
-        matrix10 = np.genfromtxt("corr10_cycle_test.csv", delimiter=",")
-        matrix20 = np.genfromtxt("corr20_cycle_test.csv", delimiter=",")
-        corr_amps = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.20, 1.4, 1.6, 1.8, 2, 2.25, 2.5, 3, 3.5, 4, 4.5, 5]
-        y1 = []
-        y1err = []
-        y10 = []
-        y10err = []
-        y20 = []
-        y20err = []
-
-        for index, cycle in enumerate(corr_amps):
-            value_list = matrix1[(index*2), :]
-            error_list = matrix1[(index*2) + 1, :]
-            y_value = mean(value_list)
-            y1.append(y_value)
-            sum_error = 0
-            for place, error in enumerate(error_list):
-                sum_error += (error/value_list[place])**2
-            sum_error = np.sqrt(sum_error)
-            y1err.append(sum_error * y_value)
-
-        for index, cycle in enumerate(corr_amps):
-            value_list = matrix10[(index*2), :]
-            error_list = matrix10[(index*2) + 1, :]
-            y_value = mean(value_list)
-            y10.append(y_value)
-            sum_error = 0
-            for place, error in enumerate(error_list):
-                sum_error += (error/value_list[place])**2
-            sum_error = np.sqrt(sum_error)
-            y10err.append(sum_error * y_value)
-
-        for index, cycle in enumerate(corr_amps):
-            value_list = matrix20[(index*2), :]
-            error_list = matrix20[(index*2) + 1, :]
-            y_value = mean(value_list)
-            y20.append(y_value)
-            sum_error = 0
-            for place, error in enumerate(error_list):
-                sum_error += (error/value_list[place])**2
-            sum_error = np.sqrt(sum_error)
-            y20err.append(sum_error * y_value)
-
-        plt.errorbar(corr_amps, y1, y1err, marker = ".", label="Cycles = 1")
-        plt.errorbar(corr_amps, y10, y10err, marker = ".", label="Cycles = 10")
-        plt.errorbar(corr_amps, y20, y20err, marker = ".", label="Cycles = 20")
-        plt.title("Corrector Amplitude test - 10 Repeats")
+    if corr_test:
+        repeats = 10
+        #As a multiplier (default 1)
+        corr_amp_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.20, 1.4, 1.6, 1.8, 2, 2.25, 2.5, 3, 3.5, 4, 4.5, 5]
+        data = np.genfromtxt(f"corr_amp_scan_repeats_{repeats}_len_{len(corr_amp_list)}.csv", delimiter=",")
+        y = []
+        y_err = []
+        for index, cycle in enumerate(corr_amp_list):
+            value_list = data[(index*2), :]
+            y.append(mean(value_list))
+            y_err.append(stdev(value_list))
+        plt.errorbar(corr_amp_list, y, y_err, marker = ".", capsize=5)
+        plt.title(f"Corrector amplitude Test - {repeats} Repeats")
+        plt.xlim(0 , max(corr_amp_list) + 0.1)
         plt.xlabel("Corrector Amplitude")
         plt.ylabel("Offset Value")
-        plt.legend()
         plt.grid(which = "both", axis = "both")
-        plt.savefig("corramptest.png", bbox_inches="tight", dpi=1200)
+        plt.savefig(f"corr_amp_scan_repeats_{repeats}_len_{len(corr_amp_list)}_plot.png", bbox_inches="tight", dpi=1200)
         plt.show()
 
+    if quad_test:
+        repeats = 10
+        #As a multiplier (default 0.01 for 1% of current value)
+        quad_amp_list = [0.001, 0.005, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05]
+        data = np.genfromtxt(f"quad_amp_scan_repeats_{repeats}_len_{len(quad_amp_list)}.csv", delimiter=",")
+        y = []
+        y_err = []
+        for index, cycle in enumerate(quad_amp_list):
+            value_list = data[(index*2), :]
+            y.append(mean(value_list))
+            y_err.append(stdev(value_list))
+        plt.errorbar(quad_amp_list, y, y_err, marker = ".", capsize=5)
+        plt.title(f"Quad amplitude Test - {repeats} Repeats")
+        plt.xlim(0 , max(quad_amp_list) + 0.005)
+        plt.xlabel("Quad Amplitude (0.01 is 1%)")
+        plt.ylabel("Offset Value")
+        plt.grid(which = "both", axis = "both")
+        plt.savefig(f"quad_amp_scan_repeats_{repeats}_len_{len(quad_amp_list)}_plot.png", bbox_inches="tight", dpi=1200)
+        plt.show()
 
     if honing_test:
-        matrix1_hon = np.genfromtxt("honing_cycle_1_test.csv", delimiter=",")
-        matrix5_hon = np.genfromtxt("honing_cycle_5_test.csv", delimiter=",")
-        matrix10_hon = np.genfromtxt("honing_cycle_10_test.csv", delimiter=",")
-        iterations = [1,2,3,4,5,6,7,8]
-        y1 = []
-        y1err = []
-        y5 = []
-        y5err = []
-        y10 = []
-        y10err = []
-
-        for index, cycle in enumerate(iterations):
-            value_list = matrix1_hon[(index*2), :]
-            error_list = matrix1_hon[(index*2) + 1, :]
-            y_value = mean(value_list)
-            y1.append(y_value)
-            sum_error = 0
-            for place, error in enumerate(error_list):
-                sum_error += (error/value_list[place])**2
-            sum_error = np.sqrt(sum_error)
-            y1err.append(sum_error * y_value)
-
-        for index, cycle in enumerate(iterations):
-            value_list = matrix5_hon[(index*2), :]
-            error_list = matrix5_hon[(index*2) + 1, :]
-            y_value = mean(value_list)
-            y5.append(y_value)
-            sum_error = 0
-            for place, error in enumerate(error_list):
-                sum_error += (error/value_list[place])**2
-            sum_error = np.sqrt(sum_error)
-            y5err.append(sum_error * y_value)
-
-        for index, cycle in enumerate(iterations):
-            value_list = matrix10_hon[(index*2), :]
-            error_list = matrix10_hon[(index*2) + 1, :]
-            y_value = mean(value_list)
-            y10.append(y_value)
-            sum_error = 0
-            for place, error in enumerate(error_list):
-                sum_error += (error/value_list[place])**2
-            sum_error = np.sqrt(sum_error)
-            y10err.append(sum_error * y_value)
-
-        plt.errorbar(iterations, y1, y1err, marker = ".", label="Cycles = 1")
-        plt.errorbar(iterations, y5, y5err, marker = ".", label="Cycles = 5")
-        plt.errorbar(iterations, y10, y10err, marker = ".", label="Cycles = 10")
-        plt.title("Honing test - 10 Repeats")
-        plt.xlabel("Iteration number")
+        #Add comparison to slow bba.
+        repeats = 5
+        cycles = 1
+        frequency = 8
+        quad_scalar = 0.01
+        corr_scalar = 1
+        data = np.genfromtxt(f"honing_simple_repeats_{repeats}_c{cycles}_f{frequency}_q{quad_scalar}_cs{corr_scalar}.csv", delimiter=",")
+        y = data[index, :]
+        y_err = data[index+1, :]
+        x_axis = [1, 2, 3, 4, 5]
+        plt.errorbar(x_axis, y, y_err, marker = ".", capsize=5)
+        plt.title(f"Honing Test")
+        plt.xlim(0 , 5.1)
+        plt.xlabel("Run number")
         plt.ylabel("Offset Value")
-        plt.legend()
         plt.grid(which = "both", axis = "both")
-        plt.savefig("honingtest.png", bbox_inches="tight", dpi=1200)
+        plt.savefig(f"honing_simple_repeats_{repeats}_c{cycles}_f{frequency}_q{quad_scalar}_cs{corr_scalar}_plot.png", bbox_inches="tight", dpi=1200)
         plt.show()
 
-    if simple:
-        matrix1 = np.genfromtxt("simple_honing_cycle_1_test.csv", delimiter=",")
-        matrix10 = np.genfromtxt("simple_honing_cycle_10_test.csv", delimiter=",")
-        steps = [1,2,3,4,5,6,7,8]
-        plt.errorbar(steps, matrix1[0, :], matrix1[1, :], marker = ".", label="Cycles = 1")
-        plt.errorbar(steps, matrix10[0, :], matrix10[1, :], marker = ".", label="Cycles = 10")
-        plt.title("Simple Honing test")
-        plt.xlabel("Iteration number")
+    if combination_test:
+        repeats = 5
+        corr_amps = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.20, 1.4, 1.6, 1.8, 2, 2.25, 2.5, 3, 3.5, 4, 4.5, 5]
+        cycles_list = [1, 10, 20]
+        for cycle in cycles_list:
+            data = np.genfromtxt(f"combination_scan_repeats_{repeats}_cycle_{cycle}.csv", delimiter=",")
+            y = []
+            y_err = []
+            for index, cycle in enumerate(corr_amps):
+                value_list = data[(index*2), :]
+                y.append(mean(value_list))
+                y_err.append(stdev(value_list))
+            plt.errorbar(corr_amps, y, y_err, marker = ".", capsize=5, label=f"Cycle={cycle}")
+        plt.title(f"Corrector amplitude and cycles Test")
+        plt.xlim(0 , 5.1)
+        plt.xlabel("Corrector amplitude")
         plt.ylabel("Offset Value")
-        plt.legend()
         plt.grid(which = "both", axis = "both")
-        plt.savefig("simplehoningtest.png", bbox_inches="tight", dpi=1200)
+        plt.savefig(f"combination_scan_repeats_{repeats}_cycle_full_plot.png", bbox_inches="tight", dpi=1200)
         plt.show()
 
 
