d6e6b8bd547609b2d60d1769e9a5b8465aa5073e
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2023-05-25 15:46:28 +0000

    Add config rework

---
+++ src/dls_bba/configuration.py
@@ -0,0 +1,70 @@
+import sys
+from dataclasses import dataclass, field
+from json import load
+from pathlib import Path
+from typing import Any, List, Optional, Union
+
+if sys.version_info > (3, 9):
+    from importlib.resources import files
+else:
+    from importlib_resources import files
+
+DEFAULT_CONFIGS = (
+    "I04-20230524-settings.json",  # Ring mode settings -> Must be first in list.
+    "defaults.json",  # UI defaults.
+)
+
+LATTICE_SETTINGS = (  # Requires update to lattice and associated components
+    "RINGMODE",
+    "UNITS",
+    "DATASOURCE",
+    "COTHREAD_CONTROL_SYSTEM_TIMEOUT",
+    "COTHREAD_CONTROL_SYSTEM_WAIT_FLAG",
+    "FOFB_NOGUI_PATH",
+    "ORBIT_RESPONSE_MATRIX_PATH",
+    "CORRECTORS_TXT_PATH",
+    "BPM2QUAD_SPECIAL_CASES",
+    "BPM2QUAD_EXCEPTIONS",
+    "QUAD2BPM_SPECIAL_CASES",
+    "QUAD2BPM_EXCEPTIONS",
+    "PSPS",
+)
+
+
+@dataclass
+class Configuration:
+    config: dict[str, Any] = field(default_factory=dict)
+
+    @classmethod
+    def from_configuration_files(cls, paths: Optional[List[Union[Path, str]]] = None):
+        config = cls()
+        config.apply_default_config()
+
+        if paths is not None:
+            config.apply_config_files(paths)
+
+        return config
+
+    def apply_default_config(self):
+        default_config_resources = [
+            Path(files("dls_bba").joinpath(resource)) for resource in DEFAULT_CONFIGS
+        ]
+
+        self.apply_config_files(default_config_resources)
+
+    def update_config(self, new_dictionary: dict) -> bool:
+        self.config.update(new_dictionary)
+        return any(key in new_dictionary for key in LATTICE_SETTINGS)
+
+    def apply_config_files(self, paths: List[Union[Path, str]]) -> bool:
+        flag = False
+
+        for pth in paths:
+            if pth is None:
+                continue
+
+            with open(pth) as f:
+                flag_files = self.update_config(load(f))
+            if flag_files:
+                flag = True
+        return flag
