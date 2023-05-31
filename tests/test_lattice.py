c09814edf747490a856ddfb50daeb1e6a0bf9b95
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2023-05-31 14:12:24 +0000

    Update tests

--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -1,20 +1,11 @@
-import sys
-from importlib.resources import files
-
 import pytest
 
 from dls_bba.lattice import Lattice
 
-if sys.version_info > (3, 9):
-    from importlib.resources import files
-else:
-    from importlib_resources import files
-
 
 @pytest.fixture(scope="module")
 def lattice_setup():
-    config_filepath = files("dls_bba").joinpath("config.json")
-    lattice = Lattice(config_folderpath=config_filepath, ringmode="I04")
+    lattice = Lattice()
     return lattice
 
 
@@ -27,29 +18,28 @@ def test_pytac_lattice_loaded_config_correctly(lattice_setup):
     assert pytac_lattice.get_default_units()[:3] in config["UNITS"].lower()
 
 
-def test_element_and_pv_lists_equal_length(lattice_setup):
+def test_element_and_name_lists_equal_length(lattice_setup):
     lattice = lattice_setup
-    assert len(lattice.bpms) == len(lattice.bpms_pvs)
-    assert len(lattice.quads) == len(lattice.quads_pvs)
-    assert len(lattice.hstrs) == len(lattice.hstrs_pvs)
-    assert len(lattice.vstrs) == len(lattice.vstrs_pvs)
+    assert len(lattice.bpms) == len(lattice.bpms_names)
+    assert len(lattice.quads) == len(lattice.quads_names)
+    assert len(lattice.hstrs) == len(lattice.hstrs_names)
+    assert len(lattice.vstrs) == len(lattice.vstrs_names)
 
 
 def test_bpm2quad(lattice_setup):
     lattice = lattice_setup
     exceptions = lattice._config["BPM2QUAD_EXCEPTIONS"]
-    for bpm_pv_1 in lattice.bpms_pvs:
-        quad_pv = lattice.bpm2quad(bpm_pv_1, pv=True)
-        if len(quad_pv) == 2:
-            bpm_pv_2a = lattice.quad2bpm(quad_pv[0], pv=True)
-            bpm_pv_2b = lattice.quad2bpm(quad_pv[1], pv=True)
-            bpm_pv_2 = [bpm_pv_2a, bpm_pv_2b]
+    for bpm_name_1 in lattice.bpms_names:
+        quad_name = lattice.bpm2quad(bpm_name_1)
+        if len(quad_name) == 2:
+            bpm_name_2a = lattice.quad2bpm(quad_name[0])
+            bpm_name_2b = lattice.quad2bpm(quad_name[1])
+            bpm_name_2 = [bpm_name_2a, bpm_name_2b]
         else:
-            bpm_pv_2 = [lattice.quad2bpm(quad_pv[0], pv=True)]
-
-        if bpm_pv_1 not in bpm_pv_2:
-            if bpm_pv_1 in exceptions:
-                if bpm_pv_2 is exceptions[bpm_pv_1]:
+            bpm_name_2 = [lattice.quad2bpm(quad_name[0])]
+        if [bpm_name_1] not in bpm_name_2:
+            if bpm_name_1 in exceptions:
+                if bpm_name_2 is exceptions[bpm_name_1]:
                     continue
                 else:
                     assert False
@@ -61,12 +51,12 @@ def test_quad2bpm(lattice_setup):
     lattice = lattice_setup
     exceptions = lattice._config["QUAD2BPM_EXCEPTIONS"]
 
-    for quad_pv_1 in lattice.quads_pvs:
-        bpm_pv = lattice.quad2bpm(quad_pv_1, pv=True)
-        quad_pv_2 = lattice.bpm2quad(bpm_pv, pv=True)
-        if quad_pv_1 not in quad_pv_2:
-            if quad_pv_1 in exceptions:
-                if quad_pv_2[0] == exceptions[quad_pv_1]:
+    for quad_name_1 in lattice.quads_names:
+        bpm_name = lattice.quad2bpm(quad_name_1)
+        quad_name_2 = lattice.bpm2quad(bpm_name[0])
+        if quad_name_1 not in quad_name_2:
+            if quad_name_1 in exceptions:
+                if quad_name_2[0] == exceptions[quad_name_1]:
                     continue
                 else:
                     assert False
@@ -74,31 +64,37 @@ def test_quad2bpm(lattice_setup):
                 assert False
 
 
-def test_element_to_pv_for_all_elements(lattice_setup):
+def test_element_to_name_for_all_elements(lattice_setup):
     lattice = lattice_setup
-    for bpm_pv in lattice.bpms_pvs:
-        element = lattice.get_element_from_pv(bpm_pv)
+    for bpm_name in lattice.bpms_names:
+        element = lattice.get_element_from_name(bpm_name)
         assert "bpm" in element.families
-    for quad_pv in lattice.quads_pvs:
-        element = lattice.get_element_from_pv(quad_pv)
+    for quad_name in lattice.quads_names:
+        element = lattice.get_element_from_name(quad_name)
         assert "quadrupole" in element.families
-    for hstr_pv in lattice.hstrs_pvs:
-        element = lattice.get_element_from_pv(hstr_pv)
+    for hstr_name in lattice.hstrs_names:
+        element = lattice.get_element_from_name(hstr_name)
         assert "hstr" in element.families
-    for vstr_pv in lattice.vstrs_pvs:
-        element = lattice.get_element_from_pv(vstr_pv)
+    for vstr_name in lattice.vstrs_names:
+        element = lattice.get_element_from_name(vstr_name)
         assert "vstr" in element.families
 
 
-def test_effective_corrector_for_elements_and_pvs(lattice_setup):
-    lattice = lattice_setup
-    bpm, bpm_pv = lattice.bpms[0], lattice.bpms_pvs[0]
+# def test_effective_corrector_for_elements_and_names(lattice_setup):
+#     lattice = lattice_setup
 
-    correctors = lattice.effective_correctors(bpm)
-    correctors_pvs = lattice.effective_correctors(bpm_pv, pv=True)
+#     for
 
-    for corrector, corrector_pv in zip(correctors, correctors_pvs):
-        assert corrector is lattice.get_element_from_pv(corrector_pv)
+
+#     bpm, bpm_name = lattice.bpms[0], lattice.bpms_names[0]
+
+#     correctors = lattice.effective_correctors(bpm)
+#     correctors_names = lattice.effective_correctors(bpm_name)
+#     print(correctors, correctors_names)
+
+#     for corrector, corrector_name in zip(correctors, correctors_names):
+#         print(corrector, corrector_name)
+#         assert corrector is lattice.get_element_from_name(corrector_name)
 
 
 # def test_corrector_kick_for_element_and_pvs(lattice_setup):
