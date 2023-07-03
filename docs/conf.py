e8200d54fc7973099dde674135b0fd594852c3a2
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2023-07-03 13:39:54 +0000

    Add numpy

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,10 @@ pygments_style = "sphinx"
 
 # This means you can link things like `str` and `asyncio` to the relevant
 # docs in the python documentation.
-intersphinx_mapping = dict(python=("https://docs.python.org/3/", None))
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
 
 # A dictionary of graphviz graph attributes for inheritance diagrams.
 inheritance_graph_attrs = dict(rankdir="TB")
