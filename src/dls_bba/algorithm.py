Fix offset calculation logic

src/dls_bba/algorithm.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
+from typing import List, Union
+
+import numpy as np
 
 from dls_bba.components import Components
 from dls_bba.datatypes import RawData, Results
-from dls_bba.lattice import Lattice
+from dls_bba.lattice import ORIGIN_SUFFIXES, Lattice
 
 
 class Algorithm(ABC):
@@ -16,3 +19,39 @@ class Algorithm(ABC):
     @abstractmethod
     def analyse(self, rawdata: RawData) -> Results:
         pass
+
+    def create_offsets_dict(
+        self, results, metadata
+    ) -> dict[str, dict[str, Union[List[float], float]]]:
+        offsets = {}
+        bpm_name = metadata["bpm_name"]
+        bpm_index = metadata["bpm_index"]
+
+        for index, axis in enumerate(["x", "y"]):
+            bpm_key = bpm_name + ORIGIN_SUFFIXES["BBA"].format(axis=axis.upper())
+            # Get current BBA offset.
+            old_bba = self._lattice.get_bba_offsets()[index][bpm_index]
+            # Calculate the change needed.
+            difference = self.calculate_new_offsets(results, axis)
+            # Calculate the new BBA offset.
+            new_bba = [old_bba + difference[0], difference[1]]
+
+        offsets[bpm_key] = {"new": new_bba, "old": old_bba, "diff": difference}
+        return offsets
+
+    def calculate_new_offsets(
+        self, results: dict[str, List[float]], axis: str
+    ) -> List[float]:
+        keys = [key for key in results.keys() if axis in key]
+        values = []
+        errors = []
+        for key in keys:
+            values.append(results[key][0])
+            errors.append(results[key][1])
+
+        sum_error = 0.0
+        mean_value = float(np.mean(values))
+        for value, error in zip(values, errors):
+            sum_error += (error / value) ** 2
+        total_error = np.sqrt(sum_error) * mean_value
+        return [mean_value, total_error]

src/dls_bba/datatypes.py
@@ -1,11 +1,8 @@
 import os
 from dataclasses import dataclass
-from typing import Any, Optional
+from typing import Any, List, Union
 
-import numpy as np
 import scipy.io as io
-
-from dls_bba.lattice import ORIGIN_SUFFIXES
 
 
 @dataclass
@@ -45,36 +42,12 @@ class Results:
         results: dict[str, Any],
         metadata: dict[str, Any],
         plotting: dict[str, Any],
-        offsets: Optional[dict[str, list[float]]] = None,
+        offsets: dict[str, dict[str, Union[List[float], float]]],
     ):
         self.results: dict = results
         self.metadata: dict = metadata
         self.plotting: dict = plotting
-        self.offsets: dict = (
-            self.find_true_bba_offsets() if offsets is None else offsets
-        )
-
-    def find_true_bba_offsets(self) -> dict[str, list[float]]:
-        offsets = {}
-        bpm_name = self.metadata["bpm_name"]
-
-        for axis in ["x", "y"]:
-            keys = [key for key in self.results.keys() if axis in key]
-            values = []
-            errors = []
-            for key in keys:
-                values.append(self.results[key][0])
-                errors.append(self.results[key][1])
-
-            sum_error = 0
-            mean_value = np.mean(values)
-            for value, error in zip(values, errors):
-                sum_error += (error / value) ** 2
-            total_error = np.sqrt(sum_error) * mean_value
-
-            bpm_key = bpm_name + ORIGIN_SUFFIXES["BBA"].format(axis=axis.upper())
-            offsets[bpm_key] = [mean_value, total_error]
-        return offsets
+        self.offsets: dict = offsets
 
     @classmethod
     def from_file(cls, filepath: str):
@@ -85,7 +58,16 @@ class Results:
         for keys, values in dct["results"].items():
             results[keys] = values.tolist()
 
-        return cls(results, dct["metadata"], dct["plotting"], dct["offsets"])
+        offsets: dict[str, dict[str, Union[List[float], float]]] = {}
+        for key, values in dct["offsets"].items():
+            offsets[key] = {}
+            for id, value in values.items():
+                if isinstance(value, float):
+                    offsets[key][id] = value
+                else:
+                    offsets[key][id] = value.tolist()
+
+        return cls(results, dct["metadata"], dct["plotting"], offsets)
 
     def save(self, folder_path):
         """"""

src/dls_bba/sbba.py
@@ -183,4 +183,6 @@ class SlowBBA(Algorithm):
                 matrix_xy = np.delete(matrix_x, stdev_list, axis=1)
                 plotting[key] = {"x": corrector_steps, "y": matrix_xy}
 
-        return Results(results, metadata, plotting)
+        offsets = self.create_offsets_dict(results, metadata)
+
+        return Results(results, metadata, plotting, offsets)