382f55d51e19944958e660dca7a490ef9142f4d8
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2023-07-07 08:12:17 +0000

    Move quad_slew_rate to lattice

--- src/dls_bba/simfbba.py
+++ src/dls_bba/simfbba.py
@@ -6,17 +6,10 @@ from cothread import Sleep
 from dls_bba.algorithm import Algorithm
 from dls_bba.components import Components
 from dls_bba.datatypes import RawData, Results
-from dls_bba.excite import (
-    NETWORK_LAG,
-    QUAD_SLEW_RATE,
-    SAFETY_NET,
-    Excitation,
-    Oscillation,
-    excite,
-)
+from dls_bba.excite import NETWORK_LAG, SAFETY_NET, Excitation, Oscillation, excite
 from dls_bba.faa import TICKS_PER_SECOND, Buffer, get_timestamp
 from dls_bba.isotime import get_isotime
-from dls_bba.lattice import Lattice
+from dls_bba.lattice import QUAD_SLEW_RATE, Lattice
 
 # To convert from nanometers to millimeters
 NM_TO_MM_UNIT_CONV = 1000000
