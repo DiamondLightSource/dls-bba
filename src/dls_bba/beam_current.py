26d44fbfff968d385a2114327fedbdead52e4a91
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2023-07-14 11:21:08 +0000

    Changes

--- src/dls_bba/beam_current.py
+++ src/dls_bba/beam_current.py
@@ -14,8 +14,21 @@ class BeamCurrentCheck:
         msg = f"Stored Starting Beam Current: {self._initial_current}"
         log.debug(msg)
 
+    def _get_topup_threshold(self):
+        """
+        An attempt to find an appropriate topup threshold.
+        300mA -> 5mA (1.6%)
+        150mA -> 4mA (2.6%)
+        50mA -> 2mA (4%)
+        10mA -> 0.5mA (5%)
+        Yields: y = 1.88337x^0.253835 - 2.922332
+        """
+        threshold = 1.88337 * self._initial_current**0.253835 - 2.922332
+        log.debug(f"Threshold: {threshold}mA")
+        return threshold
+
     def check_beam_decay(self) -> None:
-        decay_current = self._machine.config["DECAY_CURRENT"]
+        decay_current = self._get_topup_threshold()
         current_current = self._machine.get_beam_current()
         change_in_current = self._initial_current - current_current
         log.debug(f"Change in current: {change_in_current}")
@@ -42,9 +55,10 @@ class BeamCurrentCheck:
         return True
 
     def topup_beam(self) -> None:
+        start_current = self._machine.get_beam_current()
 
         while True:
-            msg = f"Please topup current to > {self._initial_current}mA."
+            msg = f"Please topup current to > {start_current}mA."
             log.error(msg)
             msg = "Input y to continue after top-up, or n to cancel: "
             response = self._machine._ask_user(msg)
@@ -56,6 +70,6 @@ class BeamCurrentCheck:
 
             elif response == "y":
                 current = self._machine.get_beam_current()
-                if self._initial_current < current:
+                if current > start_current:
                     break
         self._machine.check_feedbacks()
