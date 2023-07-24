eaae4cd52f19ff7552adc1085278d360d08b83c4
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2023-07-24 06:52:30 +0000

    Update plotting

--- src/dls_bba/plotting.py
+++ src/dls_bba/plotting.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt  # noqa E402
 from dls_bba.datatypes import CalculatedOffset, Results  # noqa E402
 from dls_bba.machine import Machine  # noqa E402
 
+# To convert from millimeters to micrometers
+MM_TO_UM_UNIT_CONV = 1000
+
 
 def bba_offsets_folder(
     machine: Machine,
@@ -36,8 +39,8 @@ def bba_offsets_plot(
     for bpm_name in machine.bba_x_pvs:
         if bpm_name in offsets_dict.keys():
             calc_offsets = offsets_dict[bpm_name]
-            change_in_x.append(calc_offsets.diff_value)
-            change_in_dx.append(calc_offsets.diff_value)
+            change_in_x.append(calc_offsets.diff_value * MM_TO_UM_UNIT_CONV)
+            change_in_dx.append(calc_offsets.diff_value * MM_TO_UM_UNIT_CONV)
         else:
             change_in_x.append(0)
             change_in_dx.append(0)
@@ -47,8 +50,8 @@ def bba_offsets_plot(
     for bpm_name in machine.bba_y_pvs:
         if bpm_name in offsets_dict.keys():
             calc_offsets = offsets_dict[bpm_name]
-            change_in_y.append(calc_offsets.diff_value)
-            change_in_dy.append(calc_offsets.diff_value)
+            change_in_y.append(calc_offsets.diff_value * MM_TO_UM_UNIT_CONV)
+            change_in_dy.append(calc_offsets.diff_value * MM_TO_UM_UNIT_CONV)
         else:
             change_in_y.append(0)
             change_in_dy.append(0)
@@ -58,11 +61,11 @@ def bba_offsets_plot(
     ax1.set_xlim(0, 174)
     ax1.axhline(y=0, color="k", linestyle="-", alpha=0.5)
     ax1.plot(change_in_x, color="b")
-    ax1.set_ylabel("Horizontal")
+    ax1.set_ylabel("Horizontal [um]")
     ax1.grid(which="both", axis="both")
     ax2.plot(change_in_y, color="r")
     ax2.axhline(y=0, color="k", linestyle="-", alpha=0.5)
-    ax2.set_ylabel("Vertical")
+    ax2.set_ylabel("Vertical [um]")
     ax2.grid(which="both", axis="both")
     fig.supxlabel("BPM Number")
     fig.supylabel("Change in BBA offset [mm]")
@@ -98,22 +101,27 @@ def bowtie_plot(filepath: str, save_location: str, save: bool = False) -> plt.fi
             if a_index == 0:
                 axs[a_index, q_index].set_title(f"{quad_name}")
             if q_index == 0:
-                axs[a_index, q_index].set_ylabel(f"Axis: {axis}")
+                axs[a_index, q_index].set_ylabel(f"Axis: {axis} [um]")
 
             color = "b" if axis == "x" else "r"
 
-            x = results_object.plotting[key]["x"]
-            y = results_object.plotting[key]["y"]
+            x = [v * MM_TO_UM_UNIT_CONV for v in results_object.plotting[key]["x"]]
+            y = [v * MM_TO_UM_UNIT_CONV for v in results_object.plotting[key]["y"]]
 
             axs[a_index, q_index].plot(x, y, color=color, lw=0.5)
 
             value, error = results_object.results[key]
+            value = value * MM_TO_UM_UNIT_CONV
+            error = error * MM_TO_UM_UNIT_CONV
+
             axs[a_index, q_index].axvline(x=value, color="k")
             axs[a_index, q_index].axvspan(
                 xmin=value - abs(error), xmax=value + abs(error), color="gray"
             )
             axs[a_index, q_index].grid(which="both", axis="both")
 
+    fig.supylabel("Oscillation Difference [um]")
+    fig.supxlabel(f"Oscillation at BPM: {bpm_name} [um]")
     plt.tight_layout()
 
     if save:
