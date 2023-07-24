424f7613432fae7545d8cd16f029accbe06a58c5
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2023-07-24 13:58:22 +0000

    Pass stderr to gui

--- src/dls_bba/logger.py
+++ src/dls_bba/logger.py
@@ -1,5 +1,6 @@
 import logging as log
 import os
+import sys
 
 CONSOLE_LOG_FORMAT = "%(levelname)-7s: [%(filename)s:%(lineno)d] — %(message)s"
 FILE_LOG_FORMAT = (
@@ -7,11 +8,31 @@ FILE_LOG_FORMAT = (
 )
 
 
+class StreamToLogger(object):
+    """
+    Fake file-like stream object that redirects writes to a logger instance.
+    """
+    def __init__(self, logger, level):
+        self.logger = logger
+        self.level = level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.level, line.rstrip())
+
+    def flush(self):
+        pass
+
+
 def get_new_logger(folder_path, gui=None):
     """"""
     logger = log.getLogger()
     logger.setLevel(log.NOTSET)
     filename = "log.log"
+
+    sys.stderr = StreamToLogger(logger, log.CRITICAL)
+
     # Console handler
     if gui is None:
         console_handler = log.StreamHandler()
