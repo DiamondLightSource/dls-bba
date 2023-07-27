b983206db9716e4c751fa379e85e988b3991e3fb
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2023-07-27 08:50:30 +0000

    Plotting cli interface

--- src/dls_bba/__main__.py
+++ src/dls_bba/__main__.py
@@ -1,3 +1,4 @@
+import os
 from argparse import ArgumentParser
 
 from dls_bba.cli import (
--- src/dls_bba/cli.py
+++ src/dls_bba/cli.py
@@ -1,16 +1,11 @@
+import os
 from typing import Any
 
 from dls_bba.algorithm import Algorithm
 from dls_bba.common import ALGORITHMS, setup_beam_based_alignment, setup_folders
 from dls_bba.components import get_component_pairs
-from dls_bba.datatypes import Results
 from dls_bba.machine import Machine
-
-# import matplotlib
-# import matplotlib.pyplot as plt
-
-
-# matplotlib.use("Qt5Agg")
+from dls_bba.plotting import bowtie_plot
 
 
 def cli_show_bpm_options(
@@ -59,38 +54,4 @@ def cli_entrypoint(
 
 def cli_quadcenter_plot(file_path: str):
     """"""
-    results_object = Results.from_file(file_path)
-
-    keys = results_object.results.keys()
-    keys = keys
-    # quad_names = []
-    # for key in keys:
-    #     quad_name = key.split("_")[0]
-    #     if quad_name not in quad_names:
-    #         quad_names.append(quad_name)
-
-    # fig, axs = plt.subplots(nrows=2, ncols=len(quad_names))
-
-    # for q_index, quad_name in enumerate(quad_names):
-    #     for a_index, axis in enumerate(["x", "y"]):
-    #         key = f"{quad_name}_{axis}"
-
-    #         if a_index == 0:
-    #             axs[a_index, q_index].set_title(f"{quad_name}")
-    #         if q_index == 0:
-    #             axs[a_index, q_index].set_ylabel(f"Axis: {axis}")
-
-    #         color = "b" if axis == "x" else "r"
-
-    #         x = results_object.plotting[key]["x"]
-    #         y = results_object.plotting[key]["y"]
-
-    #         axs[a_index, q_index].plot(x, y, color=color)
-
-    #         value, error = results_object.offsets[key]
-    #         axs[a_index, q_index].axvline(x=value, color="k")
-    #         axs[a_index, q_index].axvspan(
-    #             xmin=value - abs(error), xmax=value + abs(error), color="gray"
-    #         )
-    #         axs[a_index, q_index].grid(which="both", axis="both")
-    # plt.show()
+    bowtie_plot(file_path, os.path.dirname(file_path), True)
