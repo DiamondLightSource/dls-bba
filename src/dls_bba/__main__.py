1c9f56fb5a389864a5b117f0fca142926bdd1c58
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2023-07-27 09:24:06 +0000

    Start cli rework

--- src/dls_bba/__main__.py
+++ src/dls_bba/__main__.py
@@ -1,13 +1,12 @@
 from argparse import ArgumentParser
+import os
+from typing import List
 
-from dls_bba.cli import (
-    cli_entrypoint,
-    cli_quadcenter_plot,
-    cli_show_bpm_options,
-    cli_show_cell_options,
-)
+from dls_bba.cli import cli_entrypoint
 from dls_bba.common import ALGORITHMS
 from dls_bba.gui import start_gui
+from dls_bba.machine import Machine
+from dls_bba.plotting import bowtie_plot
 
 from . import __version__
 
@@ -15,78 +14,116 @@ __all__ = ["main"]
 
 
 def parse_arguments():
-    parser = ArgumentParser()
-    parser.add_argument("--version", "-v", action="version", version=__version__)
-    parser.add_argument(
-        "--algorithm",
-        "-a",
+    parent_parser = ArgumentParser(description="The parent parser")
+    subparsers = parent_parser.add_subparsers(title="actions")
+
+    parent_parser.add_argument("--version", "-v", action="version", version=__version__)
+
+    parser_info = subparsers.add_parser(
+        "info", parents=[parent_parser], add_help=False, description="Get information"
+    )
+    parser_info.set_defaults(which="info")
+
+    parser_run = subparsers.add_parser(
+        "run", parents=[parent_parser], add_help=False, description="Run BBA"
+    )
+    parser_run.set_defaults(which="run")
+    parser_run.add_argument(
+        "--method",
+        "-m",
         default=None,
         type=str,
         choices=ALGORITHMS.keys(),
-        help="The BBA algorithm to use.",
+        help="The algorithm to use.",
     )
-    parser.add_argument(
-        "--bpm",
-        "-b",
-        type=str,
-        help="The single BPM name that BBA should be performed on.",
+
+    parser_plot = subparsers.add_parser(
+        "plot", parents=[parent_parser], add_help=False, description="Plot results"
     )
-    parser.add_argument(
-        "--save_location",
-        "-s",
-        type=str,
-        default=None,
-        help="The location to save files to.",
-    )
-    parser.add_argument(
-        "--config",
-        "-c",
-        default=None,
-        type=list[str],
-        help="Additional configuration filepaths.",
-    )
-    parser.add_argument(
-        "--individual",
-        "-i",
-        default=None,
-        type=dict,
-        help="Additional individual configuration options",
-    )
-    parser.add_argument(
-        "--quadcenter",
-        "-q",
-        default=None,
-        type=str,
-        help="The full filepath of the xxx-results.mat file to plot.",
-    )
-    parser.add_argument(
-        "--element_names",
-        "-e",
-        action="store_true",
-        help="Display possible BPM values.",
-    )
-    parser.add_argument(
-        "--cell",
-        "-k",
-        default=None,
-        type=str,
-        help="Display the BPM values in the cell if given the identifier. Eg: '04'",
-    )
-    return parser.parse_args()
+    parser_plot.set_defaults(which="plot")
+    group = parser_plot.add_mutually_exclusive_group(required=True)
+    group.add_argument("--quadcenter", "-Q", action="store_true", help="")
+    group.add_argument("--difference", "-d", action="store_true", help="")
+
+    for subparser in [parser_info, parser_run]:
+        subparser.add_argument(
+            "--config",
+            "-c",
+            default=None,
+            type=str,
+            help="Additional configuration filepaths.",
+        )
+        subparser.add_argument(
+            "--individual",
+            "-i",
+            default=None,
+            type=dict,
+            help="Additional individual configuration options",
+        )
+
+        group = subparser.add_mutually_exclusive_group(required=True)
+        group.add_argument("--wholemachine", "-w", action="store_true", help="")
+        group.add_argument("--psps", "-p", action="store_true", help="")
+        group.add_argument("--cell", "-k", type=str, default=None, help="")
+        group.add_argument("--bpm", "-b", type=int, default=None, help="")
+        group.add_argument("--quad", "-q", type=int, default=None, help="")
+
+    for subparser in [parser_run, parser_plot]:
+        subparser.add_argument(
+            "--save_location",
+            "-s",
+            type=str,
+            default=None,
+            help="The location to save files to.",
+        )
+
+    return parent_parser.parse_args()
+
+
+def sort_elements(args) -> List[str]:
+    machine = Machine(args.config, args.individual)
+    elements = []
+
+    if args.wholemachine:
+        elements = machine.bpms_names
+    if args.psps:
+        elements = machine.psps
+    if args.cell is not None:
+        if args.cell not in machine.cell_dictionary.keys():
+            print("Invalid cell selected. Try cells '00' to '24'")
+        else:
+            elements = machine.cell_dictionary[args.cell]
+    if args.bpm is not None:
+        if (args.bpm > len(machine.bpms_names)) and (args.bpm >= 1):
+            print(f"Invalid BPM selected. Try:  1 <= BPMs <= {len(machine.bpms_names)}")
+        else:
+            elements = [machine.bpms_names[args.bpm - 1]]
+    if args.quad is not None:
+        if args.quad > len(machine.quads_names) and (args.quad >= 1):
+            print(f"Invalid Quad selected. Try:  1 <= Quads <= {len(machine.quads_names)}")
+        else:
+            elements = [machine.quads_names[args.quad - 1]]
+    return elements
 
 
 def main():
     args = parse_arguments()
-    if args.element_names:
-        cli_show_bpm_options(args.config, args.individual)
-    if args.cell is not None:
-        cli_show_cell_options(args.cell, args.config, args.individual)
-    if args.quadcenter is not None:
-        cli_quadcenter_plot(args.quadcenter)
-    if args.algorithm is not None:
+    if args.which == "info":
+        elements = sort_elements(args)
+        print(elements)
+
+    elif args.which == "run":
+        elements = sort_elements(args)
         cli_entrypoint(
-            args.algorithm, args.bpm, args.save_location, args.config, args.individual
+            args.method, elements, args.save_location, args.config, args.individual
         )
+
+    elif args.which == "plot":
+        if args.quadcenter:
+            # cli_quadcenter_plot(args.save_location)
+            bowtie_plot(args.save_location, os.path.dirname(args.save_location), True)
+        if args.difference:
+            pass
 
 
 def parse_gui_arguments(args=None):
--- src/dls_bba/cli.py
+++ src/dls_bba/cli.py
@@ -1,33 +1,9 @@
-import os
 from typing import Any
 
 from dls_bba.algorithm import Algorithm
 from dls_bba.common import ALGORITHMS, setup_beam_based_alignment, setup_folders
 from dls_bba.components import get_component_pairs
 from dls_bba.machine import Machine
-from dls_bba.plotting import bowtie_plot
-
-
-def cli_show_bpm_options(
-    extra_config_files: list[str],
-    additional_options: dict[str, Any],
-):
-    """"""
-    machine = Machine(extra_config_files, additional_options)
-    print(machine.bpms_names)
-
-
-def cli_show_cell_options(
-    cell_number: str,
-    extra_config_files: list[str],
-    additional_options: dict[str, Any],
-):
-    """"""
-    machine = Machine(extra_config_files, additional_options)
-    if cell_number not in machine.cell_dictionary.keys():
-        print("Invalid cell selected. Try cells '00' to '24'")
-    else:
-        print(machine.cell_dictionary[cell_number])
 
 
 def cli_entrypoint(
@@ -50,8 +26,3 @@ def cli_entrypoint(
     algorithm: Algorithm = ALGORITHMS[method](machine)
 
     setup_beam_based_alignment(machine, algorithm, component_pairings, save_location)
-
-
-def cli_quadcenter_plot(file_path: str):
-    """"""
-    bowtie_plot(file_path, os.path.dirname(file_path), True)
