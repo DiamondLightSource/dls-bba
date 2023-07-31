7ebb4859828a81b17484e5539b7e55d20db44923
Joshua Appleby <Joshua.Appleby@diamond.ac.uk>
2023-07-31 09:45:12 +0000

    Update logger and logging

--- src/dls_bba/logger.py
+++ src/dls_bba/logger.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 CONSOLE_LOG_FORMAT = "%(levelname)-7s: [%(filename)s:%(lineno)d] — %(message)s"
+GUI_LOG_FORMAT = "%(levelname)-7s: %(message)s"
 FILE_LOG_FORMAT = (
     "%(levelname)-7s: %(asctime)s — [%(filename)s:%(lineno)d] — %(message)s"
 )
@@ -26,22 +27,24 @@ class StreamToLogger(object):
         pass
 
 
-def get_new_logger(folder_path, gui=None):
+def get_new_logger(folder_path, gui_handler=None):
     """"""
     logger = log.getLogger()
     logger.setLevel(log.NOTSET)
+    logger.handlers.clear()
     filename = "log.log"
 
-    # Console handler
-    if gui is None:
-        console_handler = log.StreamHandler()
-    else:
-        sys.stderr = StreamToLogger(logger, log.CRITICAL)
-        console_handler = gui
-    console_handler.setLevel(log.INFO)
+    console_handler = log.StreamHandler()
+    console_handler.setLevel(log.DEBUG)
     console_handler.setFormatter(log.Formatter(CONSOLE_LOG_FORMAT))
     logger.addHandler(console_handler)
-    # File handler
+
+    if gui_handler is not None:
+        sys.stderr = StreamToLogger(logger, log.CRITICAL)
+        gui_handler.setLevel(log.INFO)
+        gui_handler.setFormatter(log.Formatter(GUI_LOG_FORMAT))
+        logger.addHandler(gui_handler)
+
     file_handler = log.FileHandler(os.path.join(folder_path, filename))
     file_handler.setLevel(log.DEBUG)
     file_handler.setFormatter(log.Formatter(FILE_LOG_FORMAT))
