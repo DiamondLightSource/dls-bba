064083865dce2ffaa68d2123a47c0f73831e9c87
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2024-08-20 11:51:50 +0100

    Use more informative name for override dict when updating machine config

--- src/dls_bba/machine.py
+++ src/dls_bba/machine.py
@@ -104,13 +104,13 @@ class Machine:
     def update_config(
         self,
         extra_config_files: Optional[List[Any]] = None,
-        dct: Optional[Dict[str, Any]] = None,
+        config_override_dict: Optional[Dict[str, Any]] = None,
     ) -> None:
         """Update the configuration files and check if a reload is required.
 
         Args:
             extra_config_files: List of extra configuration files to load.
-            dct: Dictionary of configuration overrides.
+            config_override_dict: Dictionary of configuration overrides.
         """
         flag_files = False
         flag_dict = False
@@ -118,8 +118,8 @@ class Machine:
         if extra_config_files is not None:
             flag_files = self.config.apply_config_files(extra_config_files)
 
-        if dct is not None:
-            flag_dict = self.config.update_config(dct)
+        if config_override_dict is not None:
+            flag_dict = self.config.update_config(config_override_dict)
 
         if flag_files or flag_dict:
             log.debug("Major Config Change: Reloading Lattice")
