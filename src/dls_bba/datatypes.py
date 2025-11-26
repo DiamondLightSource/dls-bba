21f6fc0091f2a1d79c9bb55fab713db49fe5b986
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2025-11-26 14:57:18 +0000

    First draft

--- src/dls_bba/datatypes.py
+++ src/dls_bba/datatypes.py
@@ -9,13 +9,47 @@ import scipy.io as io
 
 
 @dataclass
+class QuadStrength:
+    """The offset data for a single BPM PV."""
+
+    high: np.ndarray[float] = None
+    low: np.ndarray[float] = None
+
+
+@dataclass
+class CalculatedOffset:
+    """The offset data for a single BPM PV."""
+
+    old_value: float
+    new_value: float
+    diff_value: float
+    diff_error: float
+
+
+@dataclass
+class SimplifiedResults:
+    """The mean offset and standard deviation of offsets for a Quadrupole."""
+
+    mean_offset: float
+    std_offset: float
+
+
+@dataclass
+class OscillationPlane:
+    """The offset data for a single BPM PV."""
+
+    x: QuadStrength | CalculatedOffset | SimplifiedResults = None
+    y: QuadStrength | CalculatedOffset | SimplifiedResults = None
+
+
+@dataclass
 class RawData:
     """Raw data from a BBA measurement."""
 
     rawdata: Dict[str, Any]
     metadata: Dict[str, Any]
 
-    def save(self, folder_path: str) -> None:
+    def save_old(self, folder_path: str) -> None:
         """Save the RawData object to a .mat file.
 
         Can load files in MATLAB with object.("key").
@@ -40,7 +74,7 @@ class RawData:
         )
 
     @classmethod
-    def from_file(cls, filepath: str) -> RawData:
+    def old_from_old_file(cls, filepath: str) -> RawData:
         """Load a RawData object from a .mat file.
 
         Args:
@@ -52,18 +86,88 @@ class RawData:
         dct = io.loadmat(filepath, simplify_cells=True)
         return cls(dct["rawdata"], dct["metadata"])
 
+    @classmethod
+    def new_from_old_file(cls, filepath: str) -> RawData:
+        """Load a RawData object from a .mat file.
+
+        Args:
+            filepath: The path to the .mat file.
+
+        Returns:
+            A RawData object.
+        """
+        dct = io.loadmat(filepath, simplify_cells=True)
+        rawdata: Dict[str, List[float]] = {}
+        for key, values in dct["rawdata"].items():
+            quad_name, position = key.split("__")
+            quad_name = quad_name.replace("_", "-")
+            plane, quad_strength, corr_strength = position.lower().split("_")
+            if quad_name not in rawdata:
+                rawdata[quad_name] = OscillationPlane()
+            if getattr(rawdata[quad_name], plane) is None:
+                setattr(rawdata[quad_name], plane, QuadStrength)
+            raw_array = getattr(getattr(rawdata[quad_name], plane), quad_strength)
+            if raw_array is None:
+                raw_array = np.zeros((5, len(values)))
+            raw_array[int(corr_strength) - 1, :] = values
+            setattr(getattr(rawdata[quad_name], plane), quad_strength, raw_array)
+        # TODO: metadata
+        return cls(rawdata, dct["metadata"])
+
+    @classmethod
+    def new_from_new_file(cls, filepath: str) -> RawData:
+        """Load a RawData object from a .mat file.
+
+        Args:
+            filepath: The path to the .mat file.
+
+        Returns:
+            A RawData object.
+        """
+        dct = io.loadmat(filepath, simplify_cells=True)
+        rawdata: Dict[str, List[float]] = {}
+        for key, value in dct["rawdata"].items():
+            quad_name = key.replace("_", "-")
+            rawdata[quad_name] = OscillationPlane()
+            for plane, quad_data in value.items():
+                setattr(rawdata[quad_name], plane, QuadStrength)
+                for quad_strength, raw_array in quad_data.items():
+                    setattr(
+                        getattr(rawdata[quad_name], plane), quad_strength, raw_array
+                    )
+        # TODO: metadata
+        return cls(rawdata, dct["metadata"])
+
+    def save_new(self, folder_path: str) -> None:
+        """Save the RawData object to a .mat file.
+
+        Can load files in MATLAB with object.("key").
+
+        Args:
+            folder_path: The path to the folder to save the .mat file to.
+        """
+        rawdata = self.rawdata
+        metadata = self.metadata
+
+        method: str = metadata["method"]
+        isotime: str = metadata["isotime"]
+        bpm_name: str = metadata["bpm_name"]
+        filename = f"{method}-{isotime}-{bpm_name}-rawdata.mat"
+
+        raw_dict = {}
+        for key, value in rawdata.items():
+            raw_dict[key.replace("-", "_")] = value
+        dct = {"rawdata": raw_dict, "metadata": metadata}
+        io.savemat(
+            os.path.join(folder_path, filename),
+            dct,
+            oned_as="row",
+            long_field_names=True,
+        )
+
 
 @dataclass
-class CalculatedOffset:
-    """The offset data for a single BPM PV."""
-
-    old_value: float
-    new_value: float
-    diff_value: float
-    diff_error: float
-
-
-class Results:
+class FullResults:
     """The results of a BBA measurement and analysis."""
 
     def __init__(
@@ -79,7 +183,7 @@ class Results:
         self.offsets: Dict[str, CalculatedOffset] = offsets
 
     @classmethod
-    def from_file(cls, filepath: str) -> Results:
+    def old_from_old_file(cls, filepath: str) -> FullResults:
         """Load a Results object from a .mat file.
 
         Args:
@@ -100,7 +204,68 @@ class Results:
 
         return cls(results, dct["metadata"], dct["plotting"], offsets)
 
-    def save(self, folder_path: str) -> None:
+    @classmethod
+    def new_from_old_file(cls, filepath: str) -> FullResults:
+        """Load a Results object from a .mat file.
+
+        Args:
+            filepath: The path to the .mat file.
+
+        Returns:
+            The loaded Results object.
+        """
+        dct = io.loadmat(filepath, simplify_cells=True)
+
+        results: Dict[str, List[float]] = {}
+        for key, values in dct["results"].items():
+            quad_name, plane = key.split("__")
+            quad_name = quad_name.replace("_", "-")
+            if quad_name not in results.keys():
+                results[quad_name] = OscillationPlane()
+            setattr(results[quad_name], plane, SimplifiedResults(*values))
+
+        offsets: Dict[str, CalculatedOffset] = {}
+        for key, values in dct["offsets"].items():
+            bpm_name, _, plane = key.split("__")
+            bpm_name = bpm_name.replace("_", "-")
+            _, plane, _ = plane.lower().split("_")
+            if bpm_name not in offsets.keys():
+                offsets[bpm_name] = OscillationPlane()
+            setattr(offsets[bpm_name], plane, CalculatedOffset(**values))
+        # TODO: metadata & plotting
+        return cls(results, dct["metadata"], dct["plotting"], offsets)
+
+    @classmethod
+    def new_from_new_file(cls, filepath: str) -> FullResults:
+        """Load a Results object from a .mat file.
+
+        Args:
+            filepath: The path to the .mat file.
+
+        Returns:
+            The loaded Results object.
+        """
+        dct = io.loadmat(filepath, simplify_cells=True)
+
+        results: Dict[str, List[float]] = {}
+        for key, planes in dct["results"].items():
+            quad_name = key.replace("_", "-")
+            for plane, values in planes.items():
+                if quad_name not in results.keys():
+                    results[quad_name] = OscillationPlane()
+                setattr(results[quad_name], plane, SimplifiedResults(**values))
+
+        offsets: Dict[str, CalculatedOffset] = {}
+        for key, planes in dct["offsets"].items():
+            bpm_name = key.replace("_", "-")
+            for plane, values in planes.items():
+                if bpm_name not in offsets.keys():
+                    offsets[bpm_name] = OscillationPlane()
+                setattr(offsets[bpm_name], plane, CalculatedOffset(**values))
+        # TODO: metadata & plotting
+        return cls(results, dct["metadata"], dct["plotting"], offsets)
+
+    def save_old(self, folder_path: str) -> None:
         """Save the Results object to a .mat file.
 
         Can load files in MATLAB with object.("key").
@@ -124,6 +289,44 @@ class Results:
 
         dct = {
             "results": results,
+            "metadata": metadata,
+            "plotting": plotting,
+            "offsets": offsets_dict,
+        }
+        io.savemat(
+            os.path.join(folder_path, filename),
+            dct,
+            oned_as="row",
+            long_field_names=True,
+        )
+
+    def save_new(self, folder_path: str) -> None:
+        """Save the Results object to a .mat file.
+
+        Can load files in MATLAB with object.("key").
+
+        Args:
+            folder_path: The path to the folder to save the .mat file to.
+        """
+        results: Dict[str, List[float]] = self.results
+        metadata: Dict[str, Any] = self.metadata
+        plotting: Dict[str, Dict[str, np.ndarray]] = self.plotting
+        offsets: Dict[str, CalculatedOffset] = self.offsets
+
+        method: str = metadata["method"]
+        isotime: str = metadata["isotime"]
+        bpm_name: str = metadata["bpm_name"]
+        filename = f"{method}-{isotime}-{bpm_name}-results.mat"
+
+        offsets_dict: Dict[str, Dict[str, float]] = {}
+        for key, values in offsets.items():
+            offsets_dict[key.replace("-", "_")] = asdict(values)
+        results_dict = {}
+        for key, value in results.items():
+            results_dict[key.replace("-", "_")] = value
+
+        dct = {
+            "results": results_dict,
             "metadata": metadata,
             "plotting": plotting,
             "offsets": offsets_dict,
