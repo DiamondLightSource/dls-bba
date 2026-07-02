d245b051d4f1fe7f0508472d3e1b8f60cefe3a90
Phil Smith <pts@observatorysciences.co.uk>
2026-07-02 16:40:41 +0000

    Add integration test to verify fbba data

--- tests/integration/test_data_processing.py
+++ tests/integration/test_data_processing.py
@@ -1,13 +1,52 @@
 from unittest.mock import MagicMock
 
 import numpy as np
+from numpy.testing import assert_approx_equal
 from tests.conftest import TEST_DATA_DIR
 
 from dls_bba.datatypes import RawData
+from dls_bba.fbba import FastBBA
 from dls_bba.sbba import SlowBBA
 
 
-def test_rawdata_produces_correct_fulldata():
+def test_fbba_rawdata_produces_correct_fulldata():
+    """Pass rawdata from matlab file into the analysis function and verify that the
+    correct results are calculated."""
+
+    # This raw data file was initially taken in 2024 using an old data format,
+    # it was converted to the current data format for this test.
+    rawdata_path = (
+        TEST_DATA_DIR / "FastBBA-20230726T012011-SR01C-DI-EBPM-05-rawdata.mat"
+    )
+
+    machine = MagicMock()
+    fbba = FastBBA(machine)
+    rawdata = RawData.from_file(str(rawdata_path))
+    full_results = fbba.analyse(rawdata)
+
+    assert_approx_equal(
+        full_results.quad_results["SR01A-PC-Q2AB-07"]["x"].mean_offset,
+        -0.034,
+        significant=2,
+    )
+    assert_approx_equal(
+        full_results.quad_results["SR01A-PC-Q2AB-07"]["x"].std_dev_offset,
+        0.0022,
+        significant=2,
+    )
+    assert_approx_equal(
+        full_results.quad_results["SR01A-PC-Q2AB-07"]["y"].mean_offset,
+        0.0273,
+        significant=3,
+    )
+    assert_approx_equal(
+        full_results.quad_results["SR01A-PC-Q2AB-07"]["y"].std_dev_offset,
+        0.00049759,
+        significant=5,
+    )
+
+
+def test_sbba_rawdata_produces_correct_fulldata():
     """Pass our rawdata into the analysis function and verify that the
     correct results are calculated."""
 
