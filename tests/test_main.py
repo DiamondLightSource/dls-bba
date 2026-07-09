42f42a8994e797b91de1637c0c692792286b4464
Phil Smith <pts@observatorysciences.co.uk>
2026-07-09 15:46:38 +0000

    Fix tests for previous CLI changes

--- tests/test_main.py
+++ tests/test_main.py
@@ -32,25 +32,39 @@ def test_gui_entrypoint_can_provide_version():
 def test_cli_argument_shows_all_bpm_names(machine_setup):
     machine = machine_setup
     full_bpm_list = machine.bpms_names
-    cmd = [sys.executable, "-m", "dls_bba", "-e"]
+    cmd = [sys.executable, "-m", "dls_bba", "info", "-w"]
     assert subprocess.check_output(cmd).decode().strip() == str(full_bpm_list)
 
 
 def test_cli_argument_shows_full_cell_dictionary(machine_setup):
     machine = machine_setup
     full_bpm_list = machine.cell_dictionary["06"]
-    cmd = [sys.executable, "-m", "dls_bba", "-k", "06"]
+    cmd = [sys.executable, "-m", "dls_bba", "info", "-k", "06"]
     assert subprocess.check_output(cmd).decode().strip() == str(full_bpm_list)
 
 
 def test_cli_argument_fails_when_given_invalid_cell():
-    cmd = [sys.executable, "-m", "dls_bba", "-k", "25"]
-    expected_message = "Invalid cell selected. Try cells '00' to '24'"
-    assert subprocess.check_output(cmd).decode().strip() == expected_message
+    cmd = [sys.executable, "-m", "dls_bba", "info", "-k", "25"]
+    expected_message = "Invalid cell selected. Try cells '01' to '24'"
+
+    with pytest.raises(subprocess.CalledProcessError) as err:
+        subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+
+    assert expected_message in err.value.output.decode()
 
 
 def test_cli_algorithm_selection_creates_correctly_named_folder(tmp_path):
-    cmd = [sys.executable, "-m", "dls_bba", "-a", "SlowBBA", "-s", tmp_path]
+    cmd = [
+        sys.executable,
+        "-m",
+        "dls_bba",
+        "run",
+        "-a",
+        "SlowBBA",
+        "-w",
+        "-f",
+        tmp_path,
+    ]
     subprocess.call(cmd)
     foldername = [name for name in os.listdir(tmp_path) if name.startswith("SlowBBA")][
         0
