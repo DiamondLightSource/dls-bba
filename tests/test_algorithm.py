406ae0eff0a31b7c390ee15956cb6adb82a492b4
Phil Smith <pts@observatorysciences.co.uk>
2026-07-20 12:51:00 +0000

    Add basic stop/pause unit tests

---
+++ tests/test_algorithm.py
@@ -0,0 +1,40 @@
+from unittest.mock import MagicMock
+
+import cothread
+import pytest
+
+from dls_bba.algorithm import Algorithm
+
+
+class DummyAlgorithm(Algorithm):
+    def __init__(self, machine) -> None:
+        super().__init__(machine)
+
+    def run(self):
+        pass
+
+    def analyse(self):
+        pass
+
+
+def test_algorithm_stop():
+    algorithm = DummyAlgorithm(MagicMock())
+    assert not algorithm._check_stop_status()
+    algorithm.stop_run()
+    assert algorithm._check_stop_status()
+
+
+@pytest.mark.timeout(2)
+def test_algorithm_pause():
+    algorithm = DummyAlgorithm(MagicMock())
+    assert not bool(algorithm._pause_event)
+
+    def unpause():
+        cothread.Sleep(0.1)
+        algorithm.resume_run()
+
+    # Pause, spawn a second thread to unpause and then check that we unpaused
+    algorithm.pause_run()
+    assert bool(algorithm._pause_event)
+    cothread.Spawn(unpause)
+    algorithm._check_and_wait_pause_status()
