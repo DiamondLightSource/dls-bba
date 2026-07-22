da3503493f8c8746aefc935f5d66340fc28bea45
ptsOSL <ptsOSL@users.noreply.github.com>
2026-07-22 12:13:44 +0000

    deploy: 107212fd623813e555d42b2b684bbab7ccadee4d

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.2.dev155+g55fe81f11',
+    VERSION: '0.1.2.dev160+g107212fd6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
